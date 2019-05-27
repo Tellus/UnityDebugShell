20c587c50c1488dbc17f5b0df646ab82974b3388
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2019-05-27 21:30:51 +0200

    Initial commit.
    Probably won't commit anything else unless some weird and unforeseen need arises (or Unity3d shifts its API around).

---
+++ MonoBehaviourExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace UnityDebugShell
+{
+    public static class MonoBehaviourExtensions
+    {
+        /// <summary>
+        /// Shorthand for Debug.Log. <b>Also</b> logs the source class and
+        /// method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="msg"></param>
+        public static void Log(this UnityEngine.Component obj, string msg)
+        {
+            GenericLog(obj, UnityEngine.Debug.Log, msg);
+        }
+
+        public static void LogError(this UnityEngine.Component obj, string msg)
+        {
+            GenericLog(obj, UnityEngine.Debug.LogError, msg);
+        }
+
+        public static void LogWarning(this UnityEngine.Component obj, string msg)
+        {
+            GenericLog(obj, UnityEngine.Debug.LogWarning, msg);
+        }
+
+        protected delegate void LogFunction(string msg, UnityEngine.Object obj);
+
+        private static void GenericLog(UnityEngine.Component obj, LogFunction logFn, string msg)
+        {
+            // Ignore logging if not a debug build.
+            if (UnityEngine.Debug.isDebugBuild)
+            {
+                StackTrace trace = new StackTrace();
+
+                logFn($"{obj.GetType().Name}.{trace.GetFrame(2).GetMethod().Name}: {msg}", obj);
+            }
+        }
+    }
+}
